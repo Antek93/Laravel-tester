a0746ef085440f94dcc67542677ba5913d2adaf1
Antek93 <antonio.savino1993@gmail.com>
2023-05-26 14:41:39 +0200

    aggiuntiModuli

--- Modules/Prova/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('prova::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('prova.name') !!}
-    </p>
-@endsection
