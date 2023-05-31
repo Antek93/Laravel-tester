5114e80d4cf8ded3392d0be0f7277c094d4798c0
Antek93 <antonio.savino1993@gmail.com>
2023-05-31 09:38:42 +0200

    Aggiunti models Restaurant + Type

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'PIVA',
+        'image',
+        'prezzo_spedizione',
+        'user_id',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function types() {
+        return $this->belongsToMany(Type::class);
+    }
+
+}
